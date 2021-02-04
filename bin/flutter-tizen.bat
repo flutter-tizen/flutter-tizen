0321638dfd136144f6fba198292f9197053356c7
Hakkyu Kim <43136596+HakkyuKim@users.noreply.github.com>
2021-02-04 19:52:44 +0900

    Fix flutter-tizen driver bat file on windows (#35)

--- bin/flutter-tizen.bat
+++ bin/flutter-tizen.bat
@@ -10,13 +10,13 @@ where /q python3 || (
     ECHO Error: Unable to find python3 in your PATH.
     EXIT /B 1
 )
-python3 -V >NUL 2>NUL
-IF errorlevel 9009 (
+
+CALL python3 -V >NUL 2>NUL || (
     ECHO Run "python3" to install python and try again.
     EXIT /B 1
 )
 
 REM Run the flutter-tizen python script.
 SET FLUTTER_TIZEN_ROOT=%~dp0..
-python3 "%FLUTTER_TIZEN_ROOT%\bin\flutter-tizen" %*
+CALL python3 "%FLUTTER_TIZEN_ROOT%\bin\flutter-tizen" %*
 EXIT /B !ERRORLEVEL!
