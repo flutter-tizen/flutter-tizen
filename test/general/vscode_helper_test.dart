3756b40c5454d79f9f8456d86cbf156daf085740
Swift Kim <swift.kim@samsung.com>
2021-10-18 18:05:50 +0900

    Add vscode_helper_test.dart

---
+++ test/general/vscode_helper_test.dart
@@ -0,0 +1,74 @@
+// Copyright 2021 Samsung Electronics Co., Ltd. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+// @dart = 2.8
+
+import 'package:file/memory.dart';
+import 'package:file_testing/file_testing.dart';
+import 'package:flutter_tizen/vscode_helper.dart';
+import 'package:flutter_tools/src/base/file_system.dart';
+import 'package:flutter_tools/src/project.dart';
+
+import '../src/common.dart';
+
+const String _kLaunchJson = r'''
+{
+    "version": "0.2.0",
+    "configurations": [
+        {
+            "name": "flutter-tizen: Attach",
+            "request": "attach",
+            "type": "dart",
+            "deviceId": "flutter-tester",
+            "cwd": "${workspaceFolder}",
+            "observatoryUri": "http://127.0.0.1:12345"
+        }
+    ]
+}''';
+
+const String _kEmptyLaunchJson = r'''
+{
+    // This comment will be deleted.
+    "version": "0.2.0",
+    "configurations": []
+}''';
+
+void main() {
+  FileSystem fileSystem;
+
+  setUp(() {
+    fileSystem = MemoryFileSystem.test();
+  });
+
+  testWithoutContext('Can create launch.json file', () async {
+    final FlutterProject project =
+        FlutterProject.fromDirectoryTest(fileSystem.currentDirectory);
+    final Uri observatoryUri = Uri.parse('http://127.0.0.1:12345');
+
+    final File launchJsonFile =
+        project.directory.childDirectory('.vscode').childFile('launch.json');
+    expect(launchJsonFile, isNot(exists));
+
+    updateLaunchJsonFile(project, observatoryUri);
+
+    expect(launchJsonFile, exists);
+    expect(launchJsonFile.readAsStringSync(), equals(_kLaunchJson));
+  });
+
+  testWithoutContext('Can update launch.json file', () async {
+    final FlutterProject project =
+        FlutterProject.fromDirectoryTest(fileSystem.currentDirectory);
+    final Uri observatoryUri = Uri.parse('http://127.0.0.1:12345');
+
+    final File launchJsonFile =
+        project.directory.childDirectory('.vscode').childFile('launch.json');
+    launchJsonFile.createSync(recursive: true);
+    launchJsonFile.writeAsStringSync(_kEmptyLaunchJson);
+
+    updateLaunchJsonFile(project, observatoryUri);
+
+    expect(launchJsonFile, exists);
+    expect(launchJsonFile.readAsStringSync(), equals(_kLaunchJson));
+  });
+}
