78ece7482581559ce5b772f613816e331f28d668
Swift Kim <swift.kim@samsung.com>
2021-11-11 18:42:51 +0900

    Set dotnet_sort_system_directives_first to true

--- embedding/csharp/Tizen.Flutter.Embedding.Tests/Channels/BasicMessageChannelTests.cs
+++ embedding/csharp/Tizen.Flutter.Embedding.Tests/Channels/BasicMessageChannelTests.cs
@@ -2,10 +2,10 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-using NSubstitute;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
+using NSubstitute;
 using Xunit;
 
 namespace Tizen.Flutter.Embedding.Tests.Channels
