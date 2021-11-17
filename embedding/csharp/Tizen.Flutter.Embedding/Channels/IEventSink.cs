e3807c4ff6d72f625d5142e84e549b015b4f418d
WonYoung Choi <wy80.choi@samsung.com>
2021-11-17 10:01:56 +0900

    embedding/csharp: Add EventChannel and related classes (#270)
    * Add EventChannel and related classes
    
    * Fix typo
    
    Co-authored-by: Swift Kim <swift.kim@samsung.com>
    
    Co-authored-by: Swift Kim <swift.kim@samsung.com>

---
+++ embedding/csharp/Tizen.Flutter.Embedding/Channels/IEventSink.cs
@@ -0,0 +1,32 @@
+﻿// Copyright 2021 Samsung Electronics Co., Ltd. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+namespace Tizen.Flutter.Embedding
+{
+    /// <summary>
+    /// Event callback.
+    /// </summary>
+    public interface IEventSink
+    {
+        /// <summary>
+        /// Consumes a successful event.
+        /// </summary>
+        /// <param name="event">The event, possibly null.</param>
+        void Success(object @event);
+
+        /// <summary>
+        /// Consumes an error event.
+        /// </summary>
+        /// <param name="code">An error code string.</param>
+        /// <param name="message">A human-readable error message string, possibly null.</param>
+        /// <param name="details">Error details, possibly null.</param>
+        void Error(string code, string message, object details);
+
+        /// <summary>
+        /// Consumes end of stream. 
+        /// <para>Ensuing calls to <see cref="Success(object)"/> or <see cref="Error(string, string, object)"/>, if any, are ignored.</para>
+        /// </summary>
+        void EndOfStream();
+    }
+}
