d6c547c64e22a9aaf037fe7fa12d19b6a42c741b
Swift Kim <swift.kim@samsung.com>
2022-01-17 17:45:34 +0900

    Simplify

--- test/general/tizen_doctor_test.dart
+++ test/general/tizen_doctor_test.dart
@@ -9,10 +9,10 @@ import 'package:flutter_tizen/tizen_doctor.dart';
 import 'package:flutter_tizen/tizen_sdk.dart';
 import 'package:flutter_tools/src/base/file_system.dart';
 import 'package:flutter_tools/src/base/logger.dart';
+import 'package:flutter_tools/src/base/platform.dart';
 import 'package:flutter_tools/src/base/user_messages.dart';
 import 'package:flutter_tools/src/cache.dart';
 import 'package:flutter_tools/src/doctor_validator.dart';
-import 'package:test/fake.dart';
 
 import '../src/common.dart';
 import '../src/context.dart';
@@ -21,8 +21,6 @@ void main() {
   FileSystem fileSystem;
   ProcessManager processManager;
   BufferLogger logger;
-  _FakeTizenSdk tizenSdk;
-  File dotnetCli;
 
   setUpAll(() {
     Cache.flutterRoot = 'flutter';
@@ -33,16 +31,13 @@ void main() {
     processManager = FakeProcessManager.any();
     logger = BufferLogger.test();
 
-    tizenSdk = _FakeTizenSdk(fileSystem.directory('/tizen-studio'));
-    dotnetCli = fileSystem.file('dotnet');
-
     fileSystem.file('bin/internal/engine.version').createSync(recursive: true);
   });
 
   testUsingContext('Detects missing SDK', () async {
     final TizenValidator tizenValidator = TizenValidator(
       tizenSdk: null, // ignore: avoid_redundant_argument_values
-      dotnetCli: dotnetCli,
+      dotnetCli: fileSystem.file('dotnet'),
       fileSystem: fileSystem,
       logger: logger,
       processManager: processManager,
@@ -58,15 +53,17 @@ void main() {
   });
 
   testUsingContext('Detects minimum required SDK version', () async {
+    final _FakeTizenSdk tizenSdk = _FakeTizenSdk(fileSystem);
+    tizenSdk.sdkVersion = '3.7';
+
     final TizenValidator tizenValidator = TizenValidator(
       tizenSdk: tizenSdk,
-      dotnetCli: dotnetCli,
+      dotnetCli: fileSystem.file('dotnet'),
       fileSystem: fileSystem,
       logger: logger,
       processManager: processManager,
       userMessages: UserMessages(),
     );
-    tizenSdk.sdkVersion = '3.7';
 
     final ValidationResult result = await tizenValidator.validate();
     expect(result.type, equals(ValidationType.missing));
@@ -79,8 +76,8 @@ void main() {
 
   testUsingContext('Detects missing packages', () async {
     final TizenValidator tizenValidator = TizenValidator(
-      tizenSdk: tizenSdk,
-      dotnetCli: dotnetCli,
+      tizenSdk: _FakeTizenSdk(fileSystem),
+      dotnetCli: fileSystem.file('dotnet'),
       fileSystem: fileSystem,
       logger: logger,
       processManager: processManager,
@@ -96,27 +93,15 @@ void main() {
   });
 }
 
-class _FakeTizenSdk extends Fake implements TizenSdk {
-  _FakeTizenSdk(this.directory);
-
-  @override
-  final Directory directory;
-
-  @override
-  Directory get platformsDirectory => directory.childDirectory('platforms');
-
-  @override
-  Directory get toolsDirectory => directory.childDirectory('tools');
+class _FakeTizenSdk extends TizenSdk {
+  _FakeTizenSdk(FileSystem fileSystem)
+      : super(
+          fileSystem.directory('/tizen-studio'),
+          logger: BufferLogger.test(),
+          platform: FakePlatform(),
+          processManager: FakeProcessManager.any(),
+        );
 
   @override
   String sdkVersion = '4.0';
-
-  @override
-  File get tizenCli => directory.childFile('tizen');
-
-  @override
-  File get packageManagerCli => directory.childFile('package-manager-cli');
-
-  @override
-  final String defaultGccVersion = '9.2';
 }
