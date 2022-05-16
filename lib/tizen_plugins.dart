59ceae8b9aa50af2272d276f72f13d967ef18bd1
Swift Kim <swift.kim@samsung.com>
2022-05-16 14:36:43 +0900

    Migrate to flutter_lints 2.0

--- lib/tizen_plugins.dart
+++ lib/tizen_plugins.dart
@@ -177,6 +177,7 @@ const String _generatedMainTemplate = '''
 
 // ignore_for_file: avoid_classes_with_only_static_members
 // ignore_for_file: avoid_private_typedef_functions
+// ignore_for_file: depend_on_referenced_packages
 // ignore_for_file: directives_ordering
 // ignore_for_file: lines_longer_than_80_chars
 // ignore_for_file: unnecessary_cast
