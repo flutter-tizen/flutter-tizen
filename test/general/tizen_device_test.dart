81c0992ea70ffe4570c4f147afabb4a1b3be0553
Swift Kim <swift.kim@samsung.com>
2023-01-16 10:43:40 +0900

    Add more tests to tizen_device_test.dart (#476)

--- test/general/tizen_device_test.dart
+++ test/general/tizen_device_test.dart
@@ -13,6 +13,7 @@ import 'package:flutter_tools/src/device_port_forwarder.dart';
 import 'package:test/fake.dart';
 
 import '../src/common.dart';
+import '../src/context.dart';
 import '../src/fake_devices.dart';
 import '../src/fake_process_manager.dart';
 import '../src/fake_tizen_sdk.dart';
@@ -22,12 +23,13 @@ const String _kDeviceId = 'TestDeviceId';
 TizenDevice _createTizenDevice({
   ProcessManager? processManager,
   FileSystem? fileSystem,
+  Logger? logger,
 }) {
   fileSystem ??= MemoryFileSystem.test();
   return TizenDevice(
     _kDeviceId,
     modelId: 'TestModel',
-    logger: BufferLogger.test(),
+    logger: logger ?? BufferLogger.test(),
     processManager: processManager ?? FakeProcessManager.any(),
     tizenSdk: FakeTizenSdk(fileSystem),
     fileSystem: fileSystem,
@@ -41,10 +43,12 @@ List<String> _sdbCommand(List<String> args) {
 void main() {
   late FileSystem fileSystem;
   late FakeProcessManager processManager;
+  late BufferLogger logger;
 
   setUp(() {
     processManager = FakeProcessManager.empty();
     fileSystem = MemoryFileSystem.test();
+    logger = BufferLogger.test();
   });
 
   testWithoutContext('TizenDevice.startApp succeeds in debug mode', () async {
@@ -65,7 +69,7 @@ void main() {
         stdout: <String>[
           'cpu_arch:armv7',
           'secure_protocol:disabled',
-          'platform_version:4.0',
+          'platform_version:5.5',
         ].join('\n'),
       ),
       FakeCommand(
@@ -107,6 +111,111 @@ void main() {
   });
 
   testWithoutContext(
+      'TizenDevice.installApp warns if the device API version is lower than the package API version',
+      () async {
+    final TizenDevice device = _createTizenDevice(
+      processManager: processManager,
+      fileSystem: fileSystem,
+      logger: logger,
+    );
+    final TizenTpk tpk = TizenTpk(
+      applicationPackage: fileSystem.file('app.tpk')..createSync(),
+      manifest: _FakeTizenManifest(),
+    );
+
+    processManager.addCommands(<FakeCommand>[
+      FakeCommand(
+        command: _sdbCommand(<String>['capability']),
+        stdout: <String>[
+          'cpu_arch:armv7',
+          'secure_protocol:disabled',
+          'platform_version:4.0',
+        ].join('\n'),
+      ),
+      FakeCommand(
+        command: _sdbCommand(<String>['shell', 'app_launcher', '-l']),
+      ),
+      FakeCommand(command: _sdbCommand(<String>['install', 'app.tpk'])),
+    ]);
+
+    expect(await device.installApp(tpk), isTrue);
+    expect(
+      logger.warningText,
+      contains('Warning: The package API version (5.5) is greater than'),
+    );
+    expect(processManager, hasNoRemainingExpectations);
+  });
+
+  testWithoutContext(
+      'TizenDevice.installApp uninstalls and reinstalls TPK if installation fails',
+      () async {
+    final TizenDevice device = _createTizenDevice(
+      processManager: processManager,
+      fileSystem: fileSystem,
+      logger: logger,
+    );
+    final TizenTpk tpk = TizenTpk(
+      applicationPackage: fileSystem.file('app.tpk')..createSync(),
+      manifest: _FakeTizenManifest(),
+    );
+
+    processManager.addCommands(<FakeCommand>[
+      FakeCommand(
+        command: _sdbCommand(<String>['capability']),
+        stdout: <String>[
+          'cpu_arch:armv7',
+          'secure_protocol:disabled',
+          'platform_version:5.5',
+        ].join('\n'),
+      ),
+      FakeCommand(
+        command: _sdbCommand(<String>['shell', 'app_launcher', '-l']),
+        stdout: '''
+Application List for user 5001
+User's Application
+  Name    AppID
+=================================================
+'TestApp'     'TestApplication'
+''',
+      ),
+      FakeCommand(
+        command: _sdbCommand(<String>[
+          'pull',
+          '/opt/usr/apps/TestPackage/author-signature.xml',
+          '/.tmp_rand0/rand0/author-signature.xml',
+        ]),
+        onRun: () {
+          fileSystem
+              .file('/.tmp_rand0/rand0/author-signature.xml')
+              .createSync(recursive: true);
+        },
+      ),
+      FakeCommand(
+        command: _sdbCommand(<String>['install', 'app.tpk']),
+        stdout: '''
+__return_cb req_id[1] pkg_type[tpk] pkgid[TestPackage] key[install_percent] val[25]
+__return_cb req_id[1] pkg_type[tpk] pkgid[TestPackage] key[error] val[-11]
+__return_cb req_id[1] pkg_type[tpk] pkgid[TestPackage] key[end] val[fail]
+processing result : Author certificate not match [-11] failed
+''',
+      ),
+      FakeCommand(
+        command: _sdbCommand(<String>['uninstall', 'TestPackage']),
+        stdout: '''
+__return_cb req_id[1] pkg_type[tpk] pkgid[TestPackage] key[install_percent] val[100]
+__return_cb req_id[1] pkg_type[tpk] pkgid[TestPackage] key[end] val[ok]
+''',
+      ),
+      FakeCommand(command: _sdbCommand(<String>['install', 'app.tpk'])),
+    ]);
+
+    expect(await device.installApp(tpk), isTrue);
+    expect(logger.errorText, contains('Installing TPK failed'));
+    expect(logger.statusText, contains('Uninstalling old version...'));
+    expect(processManager, hasNoRemainingExpectations);
+  });
+
+  testWithoutContext(
       'TizenDevice.installApp installs TPK twice for TV emulators', () async {
     final TizenDevice device = _createTizenDevice(
       processManager: processManager,
@@ -123,7 +232,7 @@ void main() {
         stdout: <String>[
           'cpu_arch:x86',
           'secure_protocol:enabled',
-          'platform_version:4.0',
+          'platform_version:5.5',
         ].join('\n'),
       ),
       FakeCommand(command: _sdbCommand(<String>['shell', '0', 'applist'])),
@@ -207,6 +316,59 @@ void main() {
     expect(tvDevice.deviceProfile, equals('tv'));
     expect(tvDevice.isSupported(), isFalse);
   });
+
+  testWithoutContext(
+      'TizenDevicePortForwarder.forwardedPorts can list forwarded ports', () {
+    final TizenDevicePortForwarder forwarder = TizenDevicePortForwarder(
+      device: _createTizenDevice(
+        processManager: processManager,
+        fileSystem: fileSystem,
+      ),
+      logger: logger,
+    );
+    processManager.addCommand(FakeCommand(
+      command: _sdbCommand(<String>['forward', '--list']),
+      stdout: '''
+List of port forwarding
+SERIAL                  LOCAL           REMOTE
+TestDeviceId            tcp:2345        tcp:1234
+''',
+    ));
+
+    final List<ForwardedPort> forwardedPorts = forwarder.forwardedPorts;
+    expect(forwardedPorts, hasLength(1));
+    expect(forwardedPorts.first.hostPort, equals(2345));
+    expect(forwardedPorts.first.devicePort, equals(1234));
+    expect(processManager, hasNoRemainingExpectations);
+  });
+
+  testUsingContext('TizenDevice.dispose disposes the port forwarder', () async {
+    final TizenDevice device = _createTizenDevice(
+      processManager: processManager,
+      fileSystem: fileSystem,
+    );
+    processManager.addCommands(<FakeCommand>[
+      FakeCommand(
+        command: _sdbCommand(<String>['forward', 'tcp:2345', 'tcp:1234']),
+      ),
+      FakeCommand(
+        command: _sdbCommand(<String>['forward', '--list']),
+        stdout: '''
+List of port forwarding
+SERIAL                  LOCAL           REMOTE
+TestDeviceId            tcp:2345        tcp:1234
+''',
+      ),
+      FakeCommand(
+        command: _sdbCommand(<String>['forward', '--remove', 'tcp:2345']),
+      ),
+    ]);
+
+    await device.portForwarder.forward(1234, hostPort: 2345);
+    await device.dispose();
+
+    expect(processManager, hasNoRemainingExpectations);
+  });
 }
 
 class _FakeTizenManifest extends Fake implements TizenManifest {
@@ -219,5 +381,5 @@ class _FakeTizenManifest extends Fake implements TizenManifest {
   String applicationId = 'TestApplication';
 
   @override
-  String? apiVersion;
+  String? apiVersion = '5.5';
 }
