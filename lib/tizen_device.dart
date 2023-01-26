6ba4ef514f6b82acf44858f58d8a91d7d0964f16
Swift Kim <swift.kim@samsung.com>
2023-01-26 15:06:57 +0900

    enableDartProfiling can be disabled

--- lib/tizen_device.dart
+++ lib/tizen_device.dart
@@ -381,7 +381,7 @@ class TizenDevice extends Device {
     }
 
     final List<String> engineArgs = <String>[
-      '--enable-dart-profiling',
+      if (debuggingOptions.enableDartProfiling) '--enable-dart-profiling',
       if (traceStartup) '--trace-startup',
       if (route != null) ...<String>['--route', route],
       if (debuggingOptions.enableSoftwareRendering)
@@ -473,7 +473,7 @@ class TizenDevice extends Device {
   }
 
   @override
-  Future<bool> stopApp(TizenTpk app, {String? userIdentifier}) async {
+  Future<bool> stopApp(TizenTpk? app, {String? userIdentifier}) async {
     if (app == null) {
       return false;
     }
