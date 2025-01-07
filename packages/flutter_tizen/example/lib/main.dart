60062d93e6bd531a77adde1a41793da05c4b4274
JunsuChoi <jsuya.choi@samsung.com>
2025-01-07 12:20:05 +0900

    Add apiVersion to flutter-tizen.dart
    and add example code.

---
+++ packages/flutter_tizen/example/lib/main.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_tizen/flutter_tizen.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  String _isTizen = 'Unknown';
+  String _apiVersion = 'Unknown';
+
+  @override
+  initState() {
+    super.initState();
+    setState(() {
+      _isTizen = isTizen.toString();
+      _apiVersion = apiVersion;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Flutter-Tizen Plugin example app'),
+        ),
+        body: SizedBox.expand(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Text('isTizen : $_isTizen\n'),
+              Text('apiVersion : $_apiVersion\n'),
+              
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
