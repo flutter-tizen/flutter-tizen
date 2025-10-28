db2f4bf592f7df3814630533baeb516d15c32976
JunsuChoi <jsuya.choi@samsung.com>
2025-10-28 15:19:42 +0900

    Remove EvasGL renderer and ElmFlutterView

--- embedding/cpp/include/flutter.h
+++ embedding/cpp/include/flutter.h
@@ -5,7 +5,6 @@
 #ifndef FLUTTER_TIZEN_EMBEDDING_CPP_INCLUDE_FLUTTER_H_
 #define FLUTTER_TIZEN_EMBEDDING_CPP_INCLUDE_FLUTTER_H_
 
-#include "elm_flutter_view.h"
 #include "flutter_app.h"
 #include "flutter_engine.h"
 #include "flutter_service_app.h"
