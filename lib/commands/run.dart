8621404cbff93da39527069a230b5363cf4b8314
JunsuChoi <jsuya.choi@samsung.com>
2025-11-28 13:43:47 +0900

    Add TargetDevice implementation for Tizen

--- lib/commands/run.dart
+++ lib/commands/run.dart
@@ -3,10 +3,27 @@
 // found in the LICENSE file.
 
 import 'package:flutter_tools/src/commands/run.dart';
+import 'package:flutter_tools/src/device.dart';
+import 'package:flutter_tools/src/globals.dart' as globals;
 
 import '../tizen_cache.dart';
 import '../tizen_plugins.dart';
+import '../tizen_target_devices.dart';
 
 class TizenRunCommand extends RunCommand with DartPluginRegistry, TizenRequiredArtifacts {
   TizenRunCommand({super.verboseHelp});
+
+  @override
+  Future<List<Device>?> findAllTargetDevices({
+    bool includeDevicesUnsupportedByProject = false,
+  }) async {
+    return TizenTargetDevices(
+      deviceManager: globals.deviceManager!,
+      logger: globals.logger,
+      deviceConnectionInterface: deviceConnectionInterface,
+    ).findAllTargetDevices(
+      deviceDiscoveryTimeout: deviceDiscoveryTimeout,
+      includeDevicesUnsupportedByProject: includeDevicesUnsupportedByProject,
+    );
+  }
 }
