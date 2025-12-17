40300b0448020066577c48dc682dda84139ca8b4
JunsuChoi <jsuya.choi@samsung.com>
2025-12-17 19:57:22 +0900

    Upgrade to Flutter 3.38.3  (#714)

--- lib/build_targets/native_assets.dart
+++ lib/build_targets/native_assets.dart
@@ -12,80 +12,81 @@ import 'package:flutter_tools/src/build_system/exceptions.dart';
 import 'package:flutter_tools/src/build_system/targets/native_assets.dart';
 import 'package:flutter_tools/src/convert.dart';
 import 'package:flutter_tools/src/dart/package_map.dart';
+import 'package:flutter_tools/src/isolated/native_assets/dart_hook_result.dart';
 import 'package:flutter_tools/src/isolated/native_assets/native_assets.dart';
 import 'package:meta/meta.dart';
 import 'package:package_config/package_config_types.dart';
 
 /// Source: [DartBuild] in `native_assets.dart`
-abstract class DartBuild extends Target {
-  const DartBuild({@visibleForTesting FlutterNativeAssetsBuildRunner? buildRunner})
-      : _buildRunner = buildRunner;
+class TizenDartBuild extends Target {
+  const TizenDartBuild({
+    @visibleForTesting FlutterNativeAssetsBuildRunner? buildRunner,
+    this.specifiedTargetPlatform,
+  }) : _buildRunner = buildRunner;
 
   final FlutterNativeAssetsBuildRunner? _buildRunner;
+
+  /// The target OS and architecture that we are building for.
+  final TargetPlatform? specifiedTargetPlatform;
 
   @override
   Future<void> build(Environment environment) async {
     final FileSystem fileSystem = environment.fileSystem;
-    final String? nativeAssetsEnvironment = environment.defines[kNativeAssets];
+    final DartHooksResult result;
 
-    final DartBuildResult result;
-    if (nativeAssetsEnvironment == 'false') {
-      result = const DartBuildResult.empty();
-    } else {
-      final TargetPlatform targetPlatform = _getTargetPlatform();
+    final TargetPlatform targetPlatform =
+        specifiedTargetPlatform ?? _getTargetPlatformFromEnvironment(environment, name);
 
-      final File packageConfigFile = fileSystem.file(environment.packageConfigPath);
-      final PackageConfig packageConfig = await loadPackageConfigWithLogging(
-        packageConfigFile,
-        logger: environment.logger,
+    final File packageConfigFile = fileSystem.file(environment.packageConfigPath);
+    final PackageConfig packageConfig = await loadPackageConfigWithLogging(
+      packageConfigFile,
+      logger: environment.logger,
+    );
+    final Uri projectUri = environment.projectDir.uri;
+    final String? runPackageName =
+        packageConfig.packages.where((Package p) => p.root == projectUri).firstOrNull?.name;
+    if (runPackageName == null) {
+      throw StateError(
+        'Could not determine run package name. '
+        'Project path "${projectUri.toFilePath()}" did not occur as package '
+        'root in package config "${environment.packageConfigPath}". '
+        'Please report a reproduction on '
+        'https://github.com/flutter/flutter/issues/169475.',
       );
-      final Uri projectUri = environment.projectDir.uri;
-      final String? runPackageName =
-          packageConfig.packages.where((Package p) => p.root == projectUri).firstOrNull?.name;
-      if (runPackageName == null) {
-        throw StateError(
-          'Could not determine run package name. '
-          'Project path "${projectUri.toFilePath()}" did not occur as package '
-          'root in package config "${environment.packageConfigPath}". '
-          'Please report a reproduction on '
-          'https://github.com/flutter/flutter/issues/169475.',
+    }
+    final String pubspecPath = packageConfigFile.uri.resolve('../pubspec.yaml').toFilePath();
+    final String? buildModeEnvironment = environment.defines[kBuildMode];
+    if (buildModeEnvironment == null) {
+      throw MissingDefineException(kBuildMode, name);
+    }
+    final buildMode = BuildMode.fromCliName(buildModeEnvironment);
+    final bool includeDevDependencies = !buildMode.isRelease;
+    final FlutterNativeAssetsBuildRunner buildRunner = _buildRunner ??
+        FlutterNativeAssetsBuildRunnerImpl(
+          environment.packageConfigPath,
+          packageConfig,
+          fileSystem,
+          environment.logger,
+          runPackageName,
+          includeDevDependencies: includeDevDependencies,
+          pubspecPath,
         );
-      }
-      final String pubspecPath = packageConfigFile.uri.resolve('../pubspec.yaml').toFilePath();
-      final String? buildModeEnvironment = environment.defines[kBuildMode];
-      if (buildModeEnvironment == null) {
-        throw MissingDefineException(kBuildMode, name);
-      }
-      final buildMode = BuildMode.fromCliName(buildModeEnvironment);
-      final bool includeDevDependencies = !buildMode.isRelease;
-      final FlutterNativeAssetsBuildRunner buildRunner = _buildRunner ??
-          FlutterNativeAssetsBuildRunnerImpl(
-            environment.packageConfigPath,
-            packageConfig,
-            fileSystem,
-            environment.logger,
-            runPackageName,
-            includeDevDependencies: includeDevDependencies,
-            pubspecPath,
-          );
-      result = await runFlutterSpecificDartBuild(
-        environmentDefines: environment.defines,
-        buildRunner: buildRunner,
-        targetPlatform: targetPlatform,
-        projectUri: projectUri,
-        fileSystem: fileSystem,
-      );
+    result = await runFlutterSpecificHooks(
+      environmentDefines: environment.defines,
+      buildRunner: buildRunner,
+      targetPlatform: targetPlatform,
+      projectUri: projectUri,
+      fileSystem: fileSystem,
+    );
+    final File dartHookResultJsonFile = environment.buildDir.childFile(dartHookResultFilename);
+    if (!dartHookResultJsonFile.parent.existsSync()) {
+      dartHookResultJsonFile.parent.createSync(recursive: true);
     }
-
-    final File dartBuildResultJsonFile = environment.buildDir.childFile(dartBuildResultFilename);
-    if (!dartBuildResultJsonFile.parent.existsSync()) {
-      dartBuildResultJsonFile.parent.createSync(recursive: true);
-    }
-    dartBuildResultJsonFile.writeAsStringSync(json.encode(result.toJson()));
+    dartHookResultJsonFile.writeAsStringSync(json.encode(result.toJson()));
 
     final depfile = Depfile(
       <File>[for (final Uri dependency in result.dependencies) fileSystem.file(dependency)],
-      <File>[fileSystem.file(dartBuildResultJsonFile)],
+      <File>[fileSystem.file(dartHookResultJsonFile)],
     );
     final File outputDepfile = environment.buildDir.childFile(depFilename);
     if (!outputDepfile.parent.existsSync()) {
@@ -114,28 +115,32 @@ abstract class DartBuild extends Target {
   String get name => 'dart_build';
 
   @override
-  List<Source> get outputs => const <Source>[
-        Source.pattern('{BUILD_DIR}/$dartBuildResultFilename'),
-      ];
+  List<Source> get outputs => const <Source>[Source.pattern('{BUILD_DIR}/$dartHookResultFilename')];
 
   /// Dependent build [Target]s can use this to consume the result of the
-  /// [DartBuild] target.
-  static Future<DartBuildResult> loadBuildResult(Environment environment) async {
-    final File dartBuildResultJsonFile = environment.buildDir.childFile(
-      DartBuild.dartBuildResultFilename,
+  /// [TizenDartBuild] target.
+  static Future<DartHooksResult> loadHookResult(Environment environment) async {
+    final File dartHookResultJsonFile = environment.buildDir.childFile(
+      TizenDartBuild.dartHookResultFilename,
     );
-    return DartBuildResult.fromJson(
-      json.decode(dartBuildResultJsonFile.readAsStringSync()) as Map<String, Object?>,
+    if (!dartHookResultJsonFile.existsSync()) {
+      return DartHooksResult.empty();
+    }
+    return DartHooksResult.fromJson(
+      json.decode(dartHookResultJsonFile.readAsStringSync()) as Map<String, Object?>,
     );
   }
 
-  static const dartBuildResultFilename = 'dart_build_result.json';
+  @override
+  List<Target> get dependencies => <Target>[];
+
+  static const dartHookResultFilename = 'dart_build_result.json';
   static const depFilename = 'dart_build.d';
 }
 
 /// Source: [DartBuildForNative] in `native_assets.dart`
-class DartBuildForNative extends DartBuild {
-  const DartBuildForNative({@visibleForTesting super.buildRunner});
+class TizenDartBuildForNative extends TizenDartBuild {
+  const TizenDartBuildForNative({@visibleForTesting super.buildRunner});
 
   // TODO(dcharkes): Add `KernelSnapshot()` for AOT builds only when adding tree-shaking information. https://github.com/dart-lang/native/issues/153
   @override
@@ -150,16 +155,16 @@ class TizenInstallCodeAssets extends Target {
   Future<void> build(Environment environment) async {
     final Uri projectUri = environment.projectDir.uri;
     final FileSystem fileSystem = environment.fileSystem;
-    final TargetPlatform targetPlatform = _getTargetPlatform();
+    final TargetPlatform targetPlatform = _getTargetPlatformFromEnvironment(environment, name);
 
     // We fetch the result from the [DartBuild].
-    final DartBuildResult dartBuildResult = await DartBuild.loadBuildResult(environment);
+    final DartHooksResult dartHookResult = await TizenDartBuild.loadHookResult(environment);
 
     // And install/copy the code assets to the right place and create a
     // native_asset.yaml that can be used by the final AOT compilation.
     final Uri nativeAssetsFileUri = environment.buildDir.childFile(nativeAssetsFilename).uri;
     await installCodeAssets(
-      dartBuildResult: dartBuildResult,
+      dartHookResult: dartHookResult,
       environmentDefines: environment.defines,
       targetPlatform: targetPlatform,
       projectUri: projectUri,
@@ -169,7 +174,7 @@ class TizenInstallCodeAssets extends Target {
     assert(await fileSystem.file(nativeAssetsFileUri).exists());
 
     final depfile = Depfile(
-      <File>[for (final Uri file in dartBuildResult.filesToBeBundled) fileSystem.file(file)],
+      <File>[for (final Uri file in dartHookResult.filesToBeBundled) fileSystem.file(file)],
       <File>[fileSystem.file(nativeAssetsFileUri)],
     );
     final File outputDepfile = environment.buildDir.childFile(depFilename);
@@ -183,7 +188,7 @@ class TizenInstallCodeAssets extends Target {
   List<String> get depfiles => <String>[depFilename];
 
   @override
-  List<Target> get dependencies => const <Target>[DartBuildForNative()];
+  List<Target> get dependencies => const <Target>[TizenDartBuildForNative()];
 
   @override
   List<Source> get inputs => const <Source>[
@@ -205,7 +210,7 @@ class TizenInstallCodeAssets extends Target {
 }
 
 /// Source: _getTargetPlatformFromEnvironment in `native_assets.dart`
-TargetPlatform _getTargetPlatform() {
+TargetPlatform _getTargetPlatformFromEnvironment(Environment environment, String name) {
   // NOTE(jsuya) : According to tizen_build_info.dart, flutter-tizen builds use the android TargetPlatform for arm, arm64, and x64.
   //So we uses TargetPlatform.tester to use LocalPlatform's C Compiler instead of AndroidNDK.
   return TargetPlatform.tester;
