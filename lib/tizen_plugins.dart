dbeac7d93e2e2946d517c15932c64ab5d1ae5bc4
JunsuChoi <jsuya.choi@samsung.com>
2026-02-05 12:49:17 +0900

    Exclude dev_dependency Dart plugins from generated registrant
    Do not include dev_dependency Tizen Dart plugins when generating tizen/flutter/generated_main.dart.
    
    https://github.com/flutter/flutter/pull/161828

--- lib/tizen_plugins.dart
+++ lib/tizen_plugins.dart
@@ -307,7 +307,8 @@ Future<void> generateEntrypointWithPluginRegistrant(
   );
   final Uri mainUri = packageConfig.toPackageUri(mainFileUri) ?? mainFileUri;
   final List<String> dartEntrypoints = _findDartEntrypoints(mainFile);
-  final List<TizenPlugin> dartPlugins = await findTizenPlugins(project, dartOnly: true);
+  final List<TizenPlugin> dartPlugins =
+      (await findTizenPlugins(project, dartOnly: true)).where((p) => !p.isDevDependency).toList();
 
   final context = <String, Object>{
     'mainImport': mainUri.toString(),
