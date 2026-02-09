104c6643b4fa8bb6477571b4dce34f799c601a3e
JunsuChoi <jsuya.choi@samsung.com>
2026-02-09 09:56:54 +0900

    Clarify `attach` command description (#737)

--- lib/commands/attach.dart
+++ lib/commands/attach.dart
@@ -20,5 +20,5 @@ class TizenAttachCommand extends AttachCommand {
   @override
   String get description => '${super.description}\n\n'
       'For attaching to Tizen devices, the VM Service URL must be provided, e.g.\n'
-      r'`$ flutter attach --debug-url=http://127.0.0.1:43000/Swm0bjIe0ks=/`';
+      r'`$ flutter-tizen attach --debug-url=http://127.0.0.1:43000/Swm0bjIe0ks=/`';
 }
